71b29d0bfe776a772b0424db58d23b43b39e254e
Ritwik <ritwik@boringnews.co>
2022-06-10 01:17:55 +0530

    cors res

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 dotenv.config({ path: "./config.env" });
 
 require("./connection");
+
 app.use(express.json());
 app.use(require("./routes/auth"));
 app.use(require("./routes/getDetails"));
